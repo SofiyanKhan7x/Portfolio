e976370c3ed38abb2086473e26701dd7a8164ea6
SofiyanKhan7x <suffu7x@gmail.com>
2025-04-12 22:45:17 +0530

    changes in home.jsx

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -5,16 +5,17 @@ const Home = () => {
     <div className="flex items-center justify-end h-full w-full px-4 sm:px-8 md:px-16 py-10 mt-5 sm:mt-2 lg:mt-0">
       <div className="max-w-3xl">
         <p className="text-base sm:text-lg md:text-xl leading-relaxed text-justify animate-slideDown">
-          I am an MCA student at MIT Aurangabad, specializing in backend
-          development. With a strong foundation in programming languages such as
-          Java and Python, as well as web development skills in HTML and CSS, I
-          am passionate about building efficient and scalable backend systems.
-          My expertise also includes working with RDBMS for effective database
-          management. I am eager to apply my knowledge and skills to real-world
-          projects and am continuously learning to stay updated with the latest
-          technological advancements. My goal is to contribute to meaningful
-          projects and grow as a backend developer. Outside of tech, I am a NCC
-          Senior Under Officer.
+          I’m currently pursuing my post-graduation and working as a Full Stack
+          Developer. I build complete web applications—from designing responsive
+          frontends using ReactJS and Angular, to developing robust backends
+          with Spring Boot. I love writing clean, efficient code in Java and
+          continuously explore new technologies to grow my skillset. Right now,
+          I’m gaining real-world experience through an internship at K7 Codelabs
+          as a Full Stack Developer, where I work on both frontend and backend
+          development. Besides my studies and internship, I take up freelance
+          projects and enjoy turning ideas into fully functional applications.
+          With strong communication skills and a passion for problem-solving,
+          I’m confident in building projects across any tech stack.
         </p>
       </div>
 
